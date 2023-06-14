da4f8376b3f197d8f173af8a2b86fcb657980e64
Andrii Sevastianov <andrii.v.sevastianov@globallogic.com>
2023-06-14 13:48:21 +0300

    namespace fix

--- include/updater/interface/updater.hpp
+++ include/updater/interface/updater.hpp
@@ -2,9 +2,12 @@
 
 #include <string_view>
 
+namespace updater
+{
 class IUpdater
 {
 public:
     virtual void update(std::string_view filepath) = 0;
     virtual ~IUpdater() = default;
 };
+}  // namespace updater
